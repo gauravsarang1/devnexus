5bd55269ea40568cc8bf939db4644787abf8b07d
gauravsarang1 <gauravsarang223@gmail.com>
2026-02-03 19:28:08 +0530

    refacor(review): add specific route for user , project

--- backend/src/path/review/review.route.ts
+++ backend/src/path/review/review.route.ts
@@ -1,18 +1,64 @@
 import { reviewController } from "./review.controller.js";
 import { Router } from "express";
 import { validate } from "../../middleware/validate.js";
-import { ReviewValidation } from '../../validation/review/review.validation.js';
+import { ReviewValidation } from "../../validation/review/review.validation.js";
 import { requireAuth } from "../../middleware/auth.middleware.js";
 
 export const reviewRouter = Router();
 
-reviewRouter.post('/', requireAuth, validate(ReviewValidation.createReview), reviewController.createReview);
-reviewRouter.get('/', validate(ReviewValidation.getReviews), reviewController.getReviews)
-reviewRouter.get('/:reviewId', requireAuth, validate(ReviewValidation.reviewById), reviewController.getReviewById);
-reviewRouter.put('/:reviewId', requireAuth, validate(ReviewValidation.editReview), reviewController.editReview);
-reviewRouter.delete('/:reviewId', requireAuth, validate(ReviewValidation.deleteReview), reviewController.deleteReview);
+reviewRouter.post(
+    "/",
+    requireAuth,
+    validate(ReviewValidation.createReview),
+    reviewController.createReview
+);
 
-reviewRouter.get('/:targetId/:targeType', requireAuth, validate(ReviewValidation.getAllReviews), reviewController.getReviews);
-reviewRouter.get('/user/:userId/stats' ,requireAuth, validate(ReviewValidation.getStats), reviewController.getStats);
+reviewRouter.get(
+    "/",
+    validate(ReviewValidation.getReviews),
+    reviewController.getReviews
+);
 
-export default reviewRouter
+reviewRouter.get(
+    "/:reviewId",
+    requireAuth,
+    validate(ReviewValidation.reviewById),
+    reviewController.getReviewById
+);
+
+reviewRouter.put(
+    "/:reviewId",
+    requireAuth,
+    validate(ReviewValidation.editReview),
+    reviewController.editReview
+);
+
+reviewRouter.delete(
+    "/:reviewId",
+    requireAuth,
+    validate(ReviewValidation.deleteReview),
+    reviewController.deleteReview
+);
+
+reviewRouter.get(
+    "/user/:userId",
+    requireAuth,
+    validate(ReviewValidation.getAllReviews),
+    reviewController.getAllReviews
+);
+
+reviewRouter.get(
+    "/project/:projectId",
+    requireAuth,
+    validate(ReviewValidation.getAllReviews),
+    reviewController.getAllReviews
+);
+
+reviewRouter.get(
+    "/user/:userId/stats",
+    requireAuth,
+    validate(ReviewValidation.getStats),
+    reviewController.getStats
+);
+
+export default reviewRouter;
